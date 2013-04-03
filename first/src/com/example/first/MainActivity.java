1215955b5b7d754172815cb3b60653b54f29d85b
Slava Egorenkov <ve@poloniumarts.com>
2013-04-03 18:03:41 +0400

    Some new feature and another one

--- first/src/com/example/first/MainActivity.java
+++ first/src/com/example/first/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //After that I added something else
+        //Some new feature
     }
 
 
